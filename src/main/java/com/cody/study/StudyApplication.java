9b01c0ed7299dc7f9b1c6feacc57543e8bcee79a
codyli <liyou0909@163.com>
2020-06-15 15:49:35 +0800

    feat: add new java session

--- src/main/java/com/cody/study/StudyApplication.java
+++ src/main/java/com/cody/study/StudyApplication.java
@@ -3,6 +3,9 @@ package com.cody.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author cody
+ */
 @SpringBootApplication
 public class StudyApplication {
 
